9cd2545e861b205a2e343cb1ede9e393cbcc97d4
Nicole Roales <sushiwabi@gmail.com>
2018-12-15 12:54:20 -0500

    Update to headings with form data

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function changeHeading(ev) {
   ev.preventDefault();
   let catForm = ev.target;
+
   let catName = catForm.catName.value;
   let heading1 = document.querySelector('h1');
   heading1.textContent = catName;
+
+  let catAge = catForm.catAge.value;
+  let ageHeading = document.querySelector('h2.age');
+  ageHeading.textContent = catAge;
+
   catForm.reset();
 }
 
 let button = document.querySelector('button');
-let form = document.querySelector('form');
 
+let form = document.querySelector('#catForm');
 form.addEventListener('submit', changeHeading);
