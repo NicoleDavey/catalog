73d92d5f031bac0bfba885b6c1f855d9b8ca6cb4
Nicole Roales <sushiwabi@gmail.com>
2018-12-16 14:07:10 -0500

    Added weight.

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ function changeHeading(ev) {
     name: catForm.catName.value,
     age: catForm.catAge.value,
     favoriteColor: catForm.favoriteColor.value,
+    weight: catForm.weight.value,
   };
 
   // grab the list from the page
   let catList = document.querySelector('ul.catList');
 
   // add the values to the list
-  catList.innerHTML += `<li style="color: ${cat.favoriteColor}"> ${cat.name}, ${
-    cat.age
-  }`;
+  catList.innerHTML += `
+  <li style="color: ${cat.favoriteColor}"> 
+  ${cat.name}, ${cat.age} (${cat.weight} lbs)
+  </li>
+  `;
+
   // catList.innerHTML += '<p>' + catName + ', ' + catAge + '</p>';
 
   // clear the form
